ded8fb8dfacf1529952e64402bdfe4bf9f3cafd1
yigit2 <yasinyigit2534@gmail.com>
2021-01-30 11:16:01 +0300

    tablo eklendi

--- akbank_data/lib/mainPages/dashboard.dart
+++ akbank_data/lib/mainPages/dashboard.dart
@@ -1,3 +1,4 @@
+import 'package:akbank_data/mainPages/chart.dart';
 import 'package:akbank_data/repos/userRepo.dart';
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
@@ -20,37 +21,72 @@ class _DashboardState extends State<Dashboard> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        color: Colors.green[400],
-        child: Center(
-            child: PageView(
+        height: MediaQuery.of(context).size.height,
+        child: Stack(
           children: [
-            LineChart(LineChartData(
-              backgroundColor: Colors.blue[900],
-              borderData: FlBorderData(
-                show: true,
-                border: const Border(
-                  bottom: BorderSide(
-                    color: Color(0xff4e4965),
-                    width: 4,
-                  ),
-                  left: BorderSide(
-                    color: Colors.transparent,
-                  ),
-                  right: BorderSide(
-                    color: Colors.transparent,
-                  ),
-                  top: BorderSide(
-                    color: Colors.transparent,
-                  ),
+            //Green Side
+            Container(
+              height: MediaQuery.of(context).size.height / 3.5,
+              margin: EdgeInsets.symmetric(horizontal: 3),
+              decoration: BoxDecoration(
+                  color: Colors.green[600],
+                  borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(40),
+                      bottomRight: Radius.circular(40))),
+            ),
+            // Welcome Text
+            Container(
+              margin: EdgeInsets.only(
+                  top: MediaQuery.of(context).size.height / 6, left: 30),
+              child: Text(
+                'Welcome Can',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w600,
+                  fontSize: 30,
                 ),
               ),
-              minX: 0,
-              maxX: 14,
-              maxY: 4,
-              minY: 0,
-            )),
+            ),
+            //SearchBar
+            Container(
+              padding: EdgeInsets.only(left: 20),
+              decoration: BoxDecoration(boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.5),
+                  spreadRadius: 1,
+                  blurRadius: 5,
+                  offset: Offset(1, 1),
+                )
+              ], color: Colors.white, borderRadius: BorderRadius.circular(50)),
+              margin: EdgeInsets.only(
+                  top: MediaQuery.of(context).size.height / 4.2,
+                  left: 20,
+                  right: 20),
+              child: TextFormField(
+                decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: 'Search',
+                    hintStyle: TextStyle(color: Colors.green[600]),
+                    suffixIcon: IconButton(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.search,
+                        color: Colors.green[600],
+                      ),
+                    )),
+              ),
+            )
+            //The Chart
+            ,
+            Container(
+              margin: EdgeInsets.only(
+                  top: MediaQuery.of(context).size.height / 3,
+                  left: 5,
+                  right: 5),
+              child: LineChartSample1(),
+            )
           ],
-        )),
+        ),
       ),
     );
   }
